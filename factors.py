6c319045ce88a54460bad5f1e28756244fc9bcd0
biruk1122 <birukfikadu29@gmail.com>
2023-10-29 01:00:50 -0700

    updated

---
+++ factors.py
@@ -0,0 +1,27 @@
+import sys
+
+def factorize(n):
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            return i, n // i
+    return None
+
+if len(sys.argv) != 2:
+    print("Usage: factors <file>")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+
+try:
+    with open(input_file, 'r') as file:
+        for line in file:
+            n = int(line.strip())
+            factors = factorize(n)
+            if factors:
+                p, q = factors
+                print(f"{n}={p}*{q}")
+
+except FileNotFoundError:
+    print(f"Error: File '{input_file}' not found.")
+except ValueError:
+    print("Error: Invalid input in the file. All lines should be valid natural numbers greater than 1.")
---
+++ rsa.py
@@ -0,0 +1,39 @@
+import sys
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def factorize(n):
+    for i in range(2, n + 1):
+        if n % i == 0 and is_prime(i):
+            j = n // i
+            if is_prime(j):
+                return i, j
+    return None
+
+if len(sys.argv) != 2:
+    print("Usage: rsa <file>")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+
+try:
+    with open(input_file, 'r') as file:
+        for line in file:
+            n = int(line.strip())
+            factors = factorize(n)
+            if factors:
+                p, q = factors
+                print(f"{n}={p}*{q}")
+            else:
+                print(f"Unable to factorize {n} into two prime numbers.")
+
+except FileNotFoundError:
+    print(f"Error: File '{input_file}' not found.")
+except ValueError:
+    print("Error: Invalid input in the file. The file should contain a single integer.")
